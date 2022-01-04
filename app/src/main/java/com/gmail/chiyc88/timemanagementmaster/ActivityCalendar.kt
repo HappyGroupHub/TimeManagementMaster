1d0c11770eb6f14cb221cb41e0897aad1bddeadc
LD <chiyc88@gmail.com>
2022-01-04 15:30:37 +0800

    Update(LD) - Connect home and calendar navigation button

--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
@@ -1,11 +1,24 @@
 package com.gmail.chiyc88.timemanagementmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ActivityCalendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+        val btn_home = findViewById<Button>(R.id.btn_home2)
+        val btn_notify = findViewById<Button>(R.id.btn_notify2)
+        val intent_home = Intent(this, MainActivity::class.java)
+        val intent_notify = Intent(this, ActivityNotify::class.java)
+
+        btn_home.setOnClickListener {
+            startActivity(intent_home)
+        }
+        btn_notify.setOnClickListener {
+            startActivity(intent_notify)
+        }
     }
 }
--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/MainActivity.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.gmail.chiyc88.timemanagementmaster
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn_calendar = findViewById<Button>(R.id.btn_calendar)
+        val btn_notify = findViewById<Button>(R.id.btn_notify)
+        val intent_calendar = Intent(this, ActivityCalendar::class.java)
+        val intent_notify = Intent(this, ActivityNotify::class.java)
+
+        btn_calendar.setOnClickListener {
+            startActivity(intent_calendar)
+        }
+        btn_notify.setOnClickListener {
+            startActivity(intent_notify)
+        }
     }
 }
