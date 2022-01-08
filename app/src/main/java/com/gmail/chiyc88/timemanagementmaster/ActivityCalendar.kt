36e4fcc82fb767349647a731a2cc34e8292a140c
LD <chiyc88@gmail.com>
2022-01-08 18:32:54 +0800

    Update(LD) - Regenerate Fragment file but open up will still crash

--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
@@ -3,7 +3,12 @@ package com.gmail.chiyc88.timemanagementmaster
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.CalendarView
 import android.widget.ImageButton
+import androidx.fragment.app.Fragment
+import com.applandeo.materialcalendarview.EventDay
 
 class ActivityCalendar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,13 +17,24 @@ class ActivityCalendar : AppCompatActivity() {
         val ibt_main = findViewById<ImageButton>(R.id.ibt_main_calendar)
         val ibt_settings = findViewById<ImageButton>(R.id.ibt_settings_calendar)
         val intent_home = Intent(this, MainActivity::class.java)
-        val intent_notify = Intent(this, ActivitySettings::class.java)
+        val intent_settings = Intent(this, ActivitySettings::class.java)
+        val btn_addEvent = findViewById<Button>(R.id.btn_addEvent)
 
         ibt_main.setOnClickListener {
             startActivity(intent_home)
         }
         ibt_settings.setOnClickListener {
-            startActivity(intent_notify)
+            startActivity(intent_settings)
+        }
+
+        btn_addEvent.setOnClickListener {
+            val addScheduleFragmeant = AddScheduleFragment()
+            val fragmeant : Fragment? =
+            supportFragmentManager.findFragmentByTag(AddScheduleFragment::class.java.simpleName)
+            if (fragmeant !is AddScheduleFragment) {
+                supportFragmentManager.beginTransaction().add(R.id.container, addScheduleFragmeant, AddScheduleFragment::class.java.simpleName).commit()
+            }
+            btn_addEvent.visibility = View.GONE
         }
     }
 }
---
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/AddScheduleFragment.kt
@@ -0,0 +1,17 @@
+package com.gmail.chiyc88.timemanagementmaster
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class AddScheduleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_schedule, container, false)
+    }
+}
