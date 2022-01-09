933baf1c9055d1a531d3bf8b50f6a69f3133dd89
LD <chiyc88@gmail.com>
2022-01-09 21:48:18 +0800

    Update(LD) - you can now select date and take user to the addEvent page, then that date will be iconed, but the icon will disappear when add another event date

--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityAddEvent.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityAddEvent.kt
@@ -1,11 +1,31 @@
 package com.gmail.chiyc88.timemanagementmaster
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ActivityAddEvent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_event)
+
+        val selectedDate = intent.getStringExtra("selectedDate")
+        val simpleDateFormat = SimpleDateFormat("yyyy年 MM月 dd日")
+        val dateLong = simpleDateFormat.format(selectedDate?.toLong())
+        val tv_selectedDate = findViewById<TextView>(R.id.tv_selectedDate)
+        tv_selectedDate.text = String.format(dateLong)
+
+        val btn_done = findViewById<Button>(R.id.btn_done)
+
+        btn_done.setOnClickListener {
+            val b = Bundle()
+            setResult(Activity.RESULT_OK, Intent().putExtras(b))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityCalendar.kt
@@ -1,24 +1,47 @@
 package com.gmail.chiyc88.timemanagementmaster
 
+import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.CalendarView
 import android.widget.ImageButton
-import androidx.fragment.app.Fragment
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.applandeo.materialcalendarview.EventDay
+import java.util.*
 
 class ActivityCalendar : AppCompatActivity() {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.extras?.let {
+            //驗證發出對象
+            if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
+                val calendarView =
+                    findViewById<com.applandeo.materialcalendarview.CalendarView>(R.id.calendarView)
+                for (calendar in calendarView.selectedDates) {
+                    calendar.time = calendar.time
+                    val event: MutableList<EventDay> = ArrayList()
+                    /**在event陣列中新增一個元素 */
+                    event.add(EventDay(calendar, R.drawable.textview_border))
+                    runOnUiThread {
+                        /**刷新介面 */
+                        calendarView.setEvents(event)
+                    }
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
         val ibt_main = findViewById<ImageButton>(R.id.ibt_main_calendar)
         val ibt_settings = findViewById<ImageButton>(R.id.ibt_settings_calendar)
+        val btn_addEvent = findViewById<Button>(R.id.btn_addEvent)
         val intent_home = Intent(this, MainActivity::class.java)
         val intent_settings = Intent(this, ActivitySettings::class.java)
-        val btn_addEvent = findViewById<Button>(R.id.btn_addEvent)
 
         ibt_main.setOnClickListener {
             startActivity(intent_home)
@@ -27,8 +50,22 @@ class ActivityCalendar : AppCompatActivity() {
             startActivity(intent_settings)
         }
 
-        btn_addEvent.setOnClickListener {
+        val calendarView =
+            findViewById<com.applandeo.materialcalendarview.CalendarView>(R.id.calendarView)
 
+        btn_addEvent.setOnClickListener {
+            val intent_addEvent = Intent(this, ActivityAddEvent::class.java)
+
+            /**Initialize Long */
+            var selectedDate = 1L
+            /**利用forEach迴圈找出指定元素 */
+            for (calendar in calendarView.selectedDates) {
+                selectedDate = calendar.timeInMillis
+            }
+            val b = Bundle()
+            b.putString("selectedDate", selectedDate.toString())
+            intent_addEvent.putExtras(b)
+            startActivityForResult(intent_addEvent, 1)
         }
     }
 }
