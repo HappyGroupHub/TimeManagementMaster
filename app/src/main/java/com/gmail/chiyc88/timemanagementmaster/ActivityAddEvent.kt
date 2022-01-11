551621d47a495109cc289f7c6377ac79553a0b80
LD <chiyc88@gmail.com>
2022-01-11 15:12:07 +0800

    Update(LD) - Add time selector

--- app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityAddEvent.kt
+++ app/src/main/java/com/gmail/chiyc88/timemanagementmaster/ActivityAddEvent.kt
@@ -1,15 +1,23 @@
 package com.gmail.chiyc88.timemanagementmaster
 
 import android.app.Activity
+import android.app.TimePickerDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.TimePicker
+import androidx.appcompat.app.AppCompatActivity
 import java.text.SimpleDateFormat
 import java.util.*
 
-class ActivityAddEvent : AppCompatActivity() {
+class ActivityAddEvent : AppCompatActivity(), TimePickerDialog.OnTimeSetListener {
+
+    var hour = 0
+    var minute = 0
+    var savedHour = 0
+    var savedMinute = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_event)
@@ -20,12 +28,36 @@ class ActivityAddEvent : AppCompatActivity() {
         val tv_selectedDate = findViewById<TextView>(R.id.tv_selectedDate)
         tv_selectedDate.text = String.format(dateLong)
 
-        val btn_done = findViewById<Button>(R.id.btn_done)
 
+        val btn_done = findViewById<Button>(R.id.btn_done)
         btn_done.setOnClickListener {
             val b = Bundle()
             setResult(Activity.RESULT_OK, Intent().putExtras(b))
             finish()
         }
+        pickTime()
+    }
+
+    fun getTimeCalendar() {
+        val cal = Calendar.getInstance()
+        hour = cal.get(Calendar.HOUR)
+        minute = cal.get(Calendar.MINUTE)
+    }
+
+    fun pickTime() {
+        val btn_selectTime = findViewById<Button>(R.id.btn_selectTime)
+        btn_selectTime.setOnClickListener {
+            getTimeCalendar()
+
+            TimePickerDialog(this, this, hour, minute, true).show()
+
+        }
+    }
+    override fun onTimeSet(p0: TimePicker?, hourOfDay: Int, minute: Int) {
+        savedHour = hourOfDay
+        savedMinute = minute
+        val tv_time = findViewById<TextView>(R.id.tv_time)
+        tv_time.text = "$hourOfDay:$minute"
+
     }
 }
